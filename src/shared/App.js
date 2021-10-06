01a128fb89d72706cf1ff4ae1957895b0d2d34ac
hyojin-k <hyo.khj@gmail.com>
2021-10-06 09:34:16 +0900

    edit - file path
    edit - file path

--- src/shared/App.js
+++ src/shared/App.js
@@ -11,7 +11,12 @@ function App() {
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin,listPlugin]}
         initialView="dayGridMonth"
-      />
+        weekends={false}
+        events={[
+          { title: 'event 1', date: '2021-10-01' },
+          { title: 'event 2', date: '2021-10-06' }
+        ]}
+        />
     </div>
   );
 }
