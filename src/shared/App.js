e8d4523f658cdb33df2a031ff75e11f4185544b7
hyojin-k <hyo.khj@gmail.com>
2021-10-07 23:40:52 +0900

    add - Add page route
    add - Add page route

--- src/shared/App.js
+++ src/shared/App.js
@@ -4,13 +4,14 @@ import { ConnectedRouter } from 'connected-react-router';
 import {history} from '../redux/configstore';
 
 import Calendar from '../pages/Calendar'
-
+import Add from '../pages/Add'
 
 function App() {
   return (
     <React.Fragment>
       <ConnectedRouter history = {history}>
-        <Route path='/'exact component={Calendar} />
+        <Route path='/' exact component={Calendar} />
+        <Route path='/addplan' exact component={Add}/>
       </ConnectedRouter>
     </React.Fragment>
   );
