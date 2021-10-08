a48c6b6e7afa9ed7ba471016de8f6613569a8083
hyojin-k <hyo.khj@gmail.com>
2021-10-09 01:44:34 +0900

    edit - css
    edit - css

--- src/components/PlanModal.js
+++ src/components/PlanModal.js
@@ -1,8 +1,8 @@
 import React from "react";
 // import styled from "styled-components";
 import { useDispatch } from "react-redux";
-import { Modal, Button } from "react-bootstrap";
-import { actionCreators as planActions} from '../redux/modules/calendar'
+import { Modal, Button, Container } from "react-bootstrap";
+import { actionCreators as planActions } from "../redux/modules/calendar";
 
 import Text from "../elements/Text";
 
@@ -17,31 +17,37 @@ const PlanModal = ({ title, date, id, completed, show, onHide }) => {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      {/* <Container> */}
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">오늘 할 일</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>일정</h4>
-        <p>{title}</p>
-        <h4>일시</h4>
-        <p>{date} </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={()=>{
-          dispatch(planActions.deletePlanFB(id));
-          onHide();
-        }}>삭제</Button>
-        <Button
-          onClick={() => {
-            dispatch(planActions.updatePlanFB(id));
-            onHide();
-          }}
-        >
-          완료
-        </Button>
-      </Modal.Footer>
-      {/* </Container> */}
+      <Container>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            오늘 할 일
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>일정</h4>
+          <p>{title}</p>
+          <h4>일시</h4>
+          <p>{date} </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            onClick={() => {
+              dispatch(planActions.deletePlanFB(id));
+              onHide();
+            }}
+          >
+            삭제
+          </Button>
+          <Button
+            onClick={() => {
+              dispatch(planActions.updatePlanFB(id));
+              onHide();
+            }}
+          >
+            완료
+          </Button>
+        </Modal.Footer>
+      </Container>
     </Modal>
   );
 };
